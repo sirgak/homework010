00dd8396a343d2ab67499338157c4f0d8f13e8ae
sirgak <life_4_ever@mail.ru>
2022-05-20 21:56:35 +0300

    test
    test

---
+++ homework005/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace homework005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zoloto;
+            int gems;
+            int kolichestvoGems;
+            int ostatokZoloto;
+            int usersGems;
+            Console.Write("Введите количество золото ");
+            zoloto = Convert.ToInt32(Console.ReadLine());
+            gems = zoloto / 5;
+            Console.Write(" 5 золото равно " + gems + " криссталам ");
+            Console.Write("Сколько криссталов хотите купить?");
+            kolichestvoGems = Convert.ToInt32(Console.ReadLine());
+            usersGems = kolichestvoGems;
+            ostatokZoloto = Convert.ToInt32(zoloto - usersGems * 5);
+            Console.WriteLine("У вас  " + usersGems + " криссталов и " + ostatokZoloto + "золото. ");
+        }
+    }
+}
